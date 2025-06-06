f957f70b3ee059d216ccd091e08512974d22e056
Himanshu <hr@Himanshus-MacBook-Air.local>
2025-06-06 17:16:30 +0530

    init

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+  return(
+    <div className='flex justify-center items-center h-screen'>
+      <SignUp />
+    </div>
+  )
+};
+
+export default SignUpPage;
