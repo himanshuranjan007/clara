b99f989aad0b0a3e2bada77f68c434947197fa40
Himanshu <24BCS12291@cuchd.in>
2025-06-10 02:13:39 +0530

    email sync done

--- src/app/api/aurinko/callback/route.ts
+++ src/app/api/aurinko/callback/route.ts
@@ -1,11 +1,58 @@
 import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+// import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import { exchangeCodeForAccessToken, getAccountDetails, mailSync } from "~/lib/aurinko";
+import { db } from "~/server/db";
 
 
-export const GET = async (req :Request)=>{
+export const GET = async (req: NextRequest) => {
 
-    const {userId} = await auth();
-    console.log("userid is ",userId);
-    return NextResponse.json({message:"Hello World"});
+    const { userId } = await auth();
+    console.log("userid is ", userId || "no user id");
+    if (!userId) return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    const params = req.nextUrl.searchParams;
+    const status = params.get("status");
+    console.log("status is ", status);
+    if (status !== "success") {
+        return NextResponse.json({ message: "Failed to link user account" });
+    }
+
+    const code = params.get("code");
+    console.log("code is ", code);
+    if (!code) return NextResponse.json({ message: "No code provided" }, { status: 400 });
+    const token = await exchangeCodeForAccessToken(code);
+
+    if(!token) return NextResponse.json({ message: "Failed to exchange code for access token" },{ status: 400 });
+
+ 
+    console.log("aurinkoToken is ", token);
+
+    // return NextResponse.json({ message: "Success", aurinkoToken }); //for testing
+
+
+    const accountDetails = await getAccountDetails(token.accessToken);
+    if(!accountDetails) return NextResponse.json({ message: "Failed to get account details" }, { status: 400 });
+    console.log("accountDetails is ", accountDetails);
+
+    await db.account.upsert({
+        where:{
+            id:token.accountId.toString()
+        },
+        update:{
+            accessToken:token.accessToken,
+            
+        },
+        create:{
+            id:token.accountId.toString(),
+            userId,
+            email:accountDetails.email,
+            name:accountDetails.name,
+            accessToken:token.accessToken,
+        }
+    })
+
+    await mailSync(token.accessToken);
     
+    return NextResponse.redirect(new URL("/mail", req.url));
+
 }
--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -1,25 +1,116 @@
 "use server";
 
 import { auth } from "@clerk/nextjs/server";
+import axios from "axios";
 
-export const getAurinkoAuthUrl = async ( serviceType: 'Google'|'Office365') => {
+export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => {
 
     const { userId } = await auth();
 
-    if(!userId) throw new Error("UnAuthorised Login");
+    if (!userId) throw new Error("UnAuthorised Login");
 
     const params = new URLSearchParams({
-        clientId:process.env.AURINKO_CLIENT_ID!,
+        clientId: process.env.AURINKO_CLIENT_ID!,
         serviceType,
-        scope:'Mail.Read Mail.Send Mail.ReadWrite Mail.Drafts Mail.All',
-        responseType:'code',
-        returnUrl:`${process.env.NEXT_PUBLIC_URL}/api/aurinko/callback`,
+        scope: 'Mail.Read Mail.Send Mail.ReadWrite Mail.Drafts Mail.All',
+        responseType: 'code',
+
+        returnUrl: `${process.env.NEXT_PUBLIC_URL}/api/aurinko/callback`,
 
     })
-    
+
     return `https://api.aurinko.io/v1/auth/authorize?${params.toString()}`
 
 
 
 
 };
+
+export const exchangeCodeForAccessToken = async (code: string) => {
+    try {
+        // const response = await fetch(`https://api.aurinko.io/v1/auth/token/${code}`,{
+        //     method:"POST",
+        //     auth:{username:process.env.AURINKO_CLIENT_ID as string,password:process.env.AURINKO_CLIENT_SECRET as string},
+
+        // })
+        const response = await axios.post(`https://api.aurinko.io/v1/auth/token/${code}`, {}, {
+            auth: { username: process.env.AURINKO_CLIENT_ID as string, password: process.env.AURINKO_CLIENT_SECRET as string },
+        })
+        return response.data as {
+            "accountId": number,
+            "accessToken": string,
+            "userId": string,
+            "userSession": string
+        }
+    } catch (error) {
+        console.error("Error fetching Aurinko token", error);
+        throw error;
+    }
+}
+
+
+export const getAccountDetails = async (accessToken: string) => {
+    try {
+        const response = await axios.get(`https://api.aurinko.io/v1/account`, {
+            headers: {
+                "Authorization": `Bearer ${accessToken}`
+            }
+
+        })
+        return response.data as {
+
+            email: string,
+            name: string,
+            // "id": number,
+            // "parentId": number,
+            // "serviceType": string,
+            // "serviceProvider": string,
+            // "active": boolean,
+            // "tokenStatus": string,
+            // "tokenError": string,
+            // "type": string,
+            // "daemon": boolean,
+            // "loginString": string,
+            // "email": string,
+            // "email2": string,
+            // "mailboxAddress": string,
+            // "name": string,
+            // "name2": string,
+            // "serverUrl": string,
+            // "serverUrl2": string,
+            // "clientOrgId": string,
+            // "authUserId": string,
+            // "authOrgId": string,
+            // "timezone": string,
+            // "tokenIssuedAt": string,
+            // "authScopes": string[],
+            // "authNativeScopes": string[],
+            // "authObtainedAt": string,
+            // "authExpiresAt": string,
+            // "userId": string,
+            // "copyToSent": boolean
+
+        };
+    } catch (error) {
+        console.error("Error fetching Aurinko account details", error);
+        throw error;
+    }
+};
+
+export const mailSync = async (accessToken: string) => {
+    try {
+        const response = await axios.post(`https://api.aurinko.io/v1/email/sync`,{}, {
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                
+            },
+            params:{
+                "daysWithin":1,
+        }})
+        console.log("mailSync response is ", response.data);
+
+    } catch (error) {
+        console.error("Error syncing mail", error);
+        throw error;
+    }
+}
